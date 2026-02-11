185325a2cbe94247ab3e2d091afd082cfed0f9bc
Sam <sampadashrestha315@gmail.com>
2026-02-11 04:31:29 +0000

    first push

--- main.cpp
+++ main.cpp
@@ -4,8 +4,19 @@ using namespace std;
 #include "monster.h"
 
 int main(int argc, char* argv[]) {
+    monster A("Pikachu",10,15);
+    monster B("Daisuke");
 
-    monster A;
+    monster x[5]={{"sam"},{"sarah",20},{"james"},{"tom"},{"ant"}};
+
+    monster *p;
+    p=&A;
+    p->display();
+    p=new monster ("Sarina",20);
+    p->display();
+
+    p=new monster [3];
+    delete []p;
   /*
  
  monster *p,x;
--- monster.h
+++ monster.h
@@ -8,6 +8,28 @@ private:
 public:
 	void Attack(monster &);
           void heal();
-	monster(string, int,int);
+	monster(string="unknown", int=1,int=1);
+	    ~monster();
+	void display();
+
 };
+
+	void monster::display(){
+		cout<<name<<endl;
+		cout<<hp<<endl;
+		cout<<potion<<endl;
+	}
+
+	monster::~monster(){
+		cout<<"Monster "<<name<<" is gone."<<endl;
+	}
+
+  monster::monster(string n,int a, int b){
+	name=n;
+	hp=a;
+	potion=b;
+	cout<<"Moster "<<name<<" is here"<<endl;
+//	cout<<name<<" hp and potion: "<<hp<< potion<<endl;
+  }
+
 #endif
